c1e9ff79fab35604dee16771b8e8d74e4e9ec19a
Adilson Pereira <adilsonpereiradesenvolvimento@gmail.com>
2021-03-31 22:11:42 -0300

    Challenge in progress

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,9 +26,56 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>Home | spacetraveling</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href="/">
+            <h1>Como utilizar Hooks</h1>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div className={styles.info}>
+              <div>
+                <FiCalendar color="#BBBBBB" />
+                <span>15 Mar 2021</span>
+              </div>
+              <div>
+                <FiUser color="#BBBBBB" />
+                <span>Joseph Oliveira</span>
+              </div>
+            </div>
+          </a>
+
+          <a href="/">
+            <h1>Criando um app CRA do zero</h1>
+            <p>
+              Tudo sobre como criar a sua primeira aplicação utilizando Create
+              React App
+            </p>
+            <div className={styles.info}>
+              <div>
+                <FiCalendar color="#BBBBBB" />
+                <span>20 Mar 2021</span>
+              </div>
+              <div>
+                <FiUser color="#BBBBBB" />
+                <span>Danilo Vieira</span>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div className={styles.morePosts}>
+          <a href="/">Carregar mais posts</a>
+        </div>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
